5a890c3c15bae94fedc275aaf34bea8d2012ae61
André Agenor <andreagenor@icloud.com>
2017-08-04 02:39:37 -0300

    Update and bug fix

--- main.cpp
+++ main.cpp
@@ -8,9 +8,5 @@ int main(int argc, char *argv[])
     w.setColor(QColor(100, 130, 230, 255));
     w.show();
 
-//    ColorWheel *r = new ColorWheel(QColor(100, 140, 220, 255));
-//    r->setWindowTitle("Color Wheel");
-//    r->show();
-
     return a.exec();
 }
--- pmath.h
+++ pmath.h
@@ -8,20 +8,16 @@ namespace physis
 namespace math
 {
 
+/// \brief This metafunction converts degrees to radians.
+/// \param T angle in degrees.
+/// \return T angle in radians.
 template <typename T>
-///
-/// \brief Converts degrees to radians.
-/// \param angle
-/// \return
-///
 inline T degreeToRadians(const T &angle) { return angle * (M_PI / 180); }
 
+/// \brief This metafunction converts radians to degrees.
+/// \param T angle in radians.
+/// \return T angle in degrees.
 template <typename T>
-///
-/// \brief Converts radians to degrees.
-/// \param angle
-/// \return
-///
 inline T radiansToDegrees(const T &angle) { return angle * (180 / M_PI); }
 
 } // END NAMESPACE MATH
