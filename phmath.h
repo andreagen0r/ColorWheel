085cdf3399c13f3a9e136579a0ef7517789b9545
André Agenor <andreagenor@icloud.com>
2017-11-27 16:36:43 -0200

    fix math issue

--- phmath.h
+++ phmath.h
@@ -13,13 +13,19 @@ constexpr T PI = T(3.14159265358979323846264338327950288L);
 /// \param T angle in degrees.
 /// \return T angle in radians.
 template <typename T>
-constexpr inline T degreeToRadians(const T &angle) { return angle * (PI<T> / 180); }
+constexpr inline T degreeToRadians(const T angle)
+{
+    return angle * ((PI<T>) / 180);
+}
 
 /// \brief This metafunction converts radians to degrees.
 /// \param T angle in radians.
 /// \return T angle in degrees.
 template <typename T>
-constexpr inline T radiansToDegrees(const T &angle) { return angle * (180 / PI<T>); }
+constexpr inline T radiansToDegrees(const T angle)
+{
+    return angle * (180 / PI<T>);
+}
 
 } // END NAMESPACE MATH
 } // END NAMESPACE PHYSIS
